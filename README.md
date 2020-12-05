ba55f810f0ce2635554f5041567e60a9b06188e9
Sergio Fernández Celorio <scelorio1990@gmail.com>
2020-12-05 20:55:51 +0100

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+  <img src="screenshots/icon.png" alt="Icon" />
+</p>
+
+<h1 align="center">GBox</h1>
+
+----------
+
+This is an extension for [ULauncher](https://ulauncher.io/), it helps you to launch easy your Gnome boxes!
+
+| ![alt](screenshots/gbox.png)|
+|-----------------------------|
+
+## Options
+
+Just type `box` and your search. GBox will check in the box name and in the UUID.
+Enter to launch the box
+
+See the screenshots for examples
+
+You can get the UUID to your clipboard, just select and press alt-enter!
+
+----------
+
+If you like my work you can
+
+<a href="https://www.buymeacoffee.com/sergius02" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
---
+++ main.py
@@ -0,0 +1,82 @@
+import logging
+import re
+from datetime import datetime
+
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+
+logger = logging.getLogger(__name__)
+
+
+class GBox(Extension):
+
+    def __init__(self):
+        super(GBox, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+    def get_config_block(self, lines, index: int):
+        result = {}
+        i = index + 1
+        block_line = lines[i]
+        while block_line != "\n":
+            prop = block_line.split("=")
+            property_name = prop[0]
+            property_value = prop[1]
+            result[property_name] = property_value.rstrip()
+            i += 1
+            if i == len(lines):
+                break
+            block_line = lines[i]
+
+        return result
+
+    def searchBoxes(self, search_str):
+        results = []
+        pattern = re.compile(re.escape("[") + "display")
+
+        config_lines = []
+        with open("/home/sergio/.config/gnome-boxes/sources/QEMU Session") as boxesFile:
+            config_lines = boxesFile.readlines()
+
+        config_blocks = []
+        for i, line in enumerate(config_lines):
+            for match in re.finditer(pattern, line):
+                config_blocks.append(self.get_config_block(config_lines, i))
+
+        for block in config_blocks:
+            if search_str == "Start typing..." \
+                    or block["last-seen-name"].__contains__(search_str) \
+                    or block["uuid"].__contains__(search_str):
+                last_access = datetime.fromtimestamp(int(block["access-last-time"]) / 1000000)
+                results.append(
+                    ExtensionResultItem(
+                        icon='images/icon.png',
+                        name=block["last-seen-name"],
+                        description="UUID: " + block["uuid"] +
+                                    "\n" + block["access-ntimes"] + " access, last " + last_access.__str__(),
+                        on_enter=RunScriptAction(
+                            'gnome-boxes --open-uuid %s &' % block["uuid"],
+                            []
+                        ),
+                        on_alt_enter=CopyToClipboardAction(block["uuid"])
+                    )
+                )
+
+        return results
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+        text = event.get_argument() or "Start typing..."
+        return RenderResultListAction(extension.searchBoxes(text))
+
+
+if __name__ == '__main__':
+    GBox().run()
